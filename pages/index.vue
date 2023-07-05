01bcdd7e42dcd72c2093f21e4eaf517583f0a539
Nizii <nizam.oezdemir@gmx.ch>
2023-07-05 16:44:04 +0200

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HeaderSection/>
+    <ContentSection/>
+  </div>
+</template>
+
+<script>
+import HeaderSection from '~/components/HeaderSection.vue';
+export default {
+  name: 'IndexPage',
+  components: {
+    HeaderSection,
+    ContentSection,
+  }
+}
+</script>
